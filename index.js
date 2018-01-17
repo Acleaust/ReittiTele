da35dc4012f44782ce90301910176d6a4cfb90fe
Acleaust <acleaust@gmail.com>
2018-01-17 21:04:27 +0200

    Testausta

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const TeleBot = require('telebot');
-const bot = new TeleBot('TELEGRAM_BOT_TOKEN');
+const bot = new TeleBot('BotToken'); //'BotToken'
+
+//
+
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Moro!')); //Testi
+
+
+bot.start();
