875b6e2afeb4a7abee06dad3d8b8fcfda845253f
Acleaust <acleaust@gmail.com>
2018-01-17 22:54:35 +0200

    Testattu muutamaa komentoa

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('BotToken'); //'BotToken'
 
+
 //
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Moro!')); //Testi
+bot.on('/start', (msg) => {
+    return bot.sendMessage(msg.from.id, `Hei, ${ msg.from.first_name }!`); //Vastaa kun käyttäjä käyttää /start komentoa
+  });
+
+// On every text message
+bot.on('text', msg => {
+    let id = msg.from.id;
+    let text = msg.text;
+
+
+
+    return bot.sendMessage(id, `Pysäkkiä "${ text }" ei löydy xd`);
+});
 
 
 bot.start();
+
+
+
