87ad36b99d4183830a3e489a9029509528174d8e
Acleaust <s1601379@student.businesscollege.fi>
2018-01-18 09:42:22 +0200

    graphGL tulostaa dataa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('BotToken'); //'BotToken'
-
+const { request } = require('graphql-request')
 
 //
 
@@ -13,9 +13,22 @@ bot.on('text', msg => {
     let id = msg.from.id;
     let text = msg.text;
 
-
+    const query = `{
+        stops(name: "${ text }") {
+            id
+            name
+            wheelchairBoarding
+        }
+      }`
+        
+      request('http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', query).then(data => console.log(data))
+    
+      if(){
+        
+      }
 
     return bot.sendMessage(id, `Pysäkkiä "${ text }" ei löydy xd`);
+
 });
 
 
