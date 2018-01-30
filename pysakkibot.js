07af1a0ccd5594c55e4d172d48a00ea0a9a1132d
Inkiläinen Alex <s1601379@student.businesscollege.fi>
2018-01-30 18:25:05 +0200

    Lisää location juttui

--- pysakkibot.js
+++ pysakkibot.js
@@ -19,12 +19,56 @@ bot.on('/start', (msg) => {
     return bot.sendMessage(msg.from.id, `Hei, ${msg.from.first_name}! Tervetuloa käyttämään pysäkkibottia!\nBotti on tällä hetkellä kesken, joten toiminnallisuutta ei vielä ole.\n\nVoit aloittaa käytön kirjoittamalla pysäkin nimen tai sen koodin (esim: "Keilaniemi" tai "E4017").`); //Vastaa kun käyttäjä käyttää /start komentoa
 });
 
-var aika = TimeFormat.fromS(81000, 'hh:mm') 
+var aika = TimeFormat.fromS(49663, 'hh:mm:ss') 
 console.log(aika)
 
 //Koko "pääohjelma"
 
 //Käyttäjän sijainnista
+bot.on(['location', 'contact'], (msg, self) => {
+    let id = msg.from.id;
+    let text = msg.text;
+    let sijainti = msg.location;
+
+    var latitude = jp.query(sijainti, '$..latitude')
+    var longitude = jp.query(sijainti, '$..longitude')
+
+    const querygetlocation = `{
+        places: nearest(
+        lat: ${latitude},
+        lon: ${longitude},
+        maxDistance: 200,
+        filterByPlaceTypes: DEPARTURE_ROW,
+        ) {
+          edges {
+            node {
+              distance
+              place{
+                id
+                __typename
+                ... on DepartureRow {
+                stoptimes (numberOfDepartures: 5) {
+                pickupType
+                realtimeDeparture
+                headsign
+                
+                }
+              }
+            }
+          }
+        }
+      }
+      }`
+
+    //Hakulauseen suoritus
+    return request(digiAPI, querygetlocation)
+      .then(function (data) {
+          var locvastaus = JSON.stringify(data);
+    
+    return bot.sendMessage(msg.from.id, `Sijaintisi on ${ locvastaus }.`);
+})});
+
+
 
 // Etsii jokaisesta viestistä pysäkin nimeä
 bot.on('text', msg => {
@@ -53,11 +97,11 @@ bot.on('text', msg => {
                     return bot.sendMessage(id, `Pysäkkiä "${text}" ei valitettavasti löydy.`);
                 }else{
                     //Hakee pyäkit ja koodit niille
-                    var pysakit = jp.query(data, '$..name');
+                    var pysakit = jp.query(data, '$..name')
                     var koodit = jp.query(data, '$..code')
                     //Erittelee pysäkit ja yhdistää koodit
                     for (i = 0; i < pysakit.length; i += 1) {
-                        var pk = pysakit[i]+" "+koodit[i]+"\n"
+                        var pk = pysakit[i]+" "+koodit[i]+""
                         console.log(pk);
                         //Tallentaa muuttujaan pysäkit + koodit viestiä varten
                         if(pysakkivalinta == null){
@@ -82,4 +126,8 @@ bot.on('text', function (msg) {
     console.log(`[text] ${msg.chat.id} ${msg.text}`);
 });
 
+//Ohjelman pyöritys
 bot.start();
+
+//Queryt
+
